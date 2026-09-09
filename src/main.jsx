cb109c68e639d8acf1aa00269ac9834b17b7f3f1
Astekasz <armando_rocha_@hotmail.com>
2026-09-09 09:51:07 -0300

    feat: vincula botões de degravação às aulas

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import PasswordRecovery from './PasswordRecovery'
 import TranscriptLibrary from './TranscriptLibrary'
+import LessonTranscriptButtons from './LessonTranscriptButtons'
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
     <PasswordRecovery />
+    <LessonTranscriptButtons />
     <TranscriptLibrary />
   </React.StrictMode>,
 )
