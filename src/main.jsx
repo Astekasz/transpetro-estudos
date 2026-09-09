15cd3b92d858954f6b603cb7737ba0c7499ddb83
Astekasz <armando_rocha_@hotmail.com>
2026-09-09 09:32:50 -0300

    feat: integra recuperação de senha

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import PasswordRecovery from './PasswordRecovery'
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <PasswordRecovery />
   </React.StrictMode>,
 )
