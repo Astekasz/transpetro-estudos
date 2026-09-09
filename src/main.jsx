38188af8cd0648987b3a94ae879e62cfde8e5544
Astekasz <armando_rocha_@hotmail.com>
2026-09-09 10:05:00 -0300

    feat: ativa balanceamento de respostas A-E

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App'
 import PasswordRecovery from './PasswordRecovery'
 import TranscriptLibrary from './TranscriptLibrary'
 import LessonTranscriptButtons from './LessonTranscriptButtons'
+import AnswerBalancer from './AnswerBalancer'
 import './styles.css'
 import './lessonTranscriptButtons.css'
 
@@ -13,5 +14,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <PasswordRecovery />
     <LessonTranscriptButtons />
     <TranscriptLibrary />
+    <AnswerBalancer />
   </React.StrictMode>,
 )
