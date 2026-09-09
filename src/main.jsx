70babc3fd64e259c2a441ddc9d85756c672ce866
Astekasz <armando_rocha_@hotmail.com>
2026-09-09 09:45:27 -0300

    feat: integra biblioteca de degravações

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import PasswordRecovery from './PasswordRecovery'
+import TranscriptLibrary from './TranscriptLibrary'
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
     <PasswordRecovery />
+    <TranscriptLibrary />
   </React.StrictMode>,
 )
