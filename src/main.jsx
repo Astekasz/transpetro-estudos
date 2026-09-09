df52e946d82a28ef7f20bd7b1357db6dc78ed094
Astekasz <armando_rocha_@hotmail.com>
2026-09-09 10:55:23 -0300

    feat: integra anotações independentes por aula

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,11 @@ import App from './App'
 import PasswordRecovery from './PasswordRecovery'
 import TranscriptLibrary from './TranscriptLibrary'
 import LessonTranscriptButtons from './LessonTranscriptButtons'
+import LessonNotes from './LessonNotes'
 import AnswerBalancer from './AnswerBalancer'
 import './styles.css'
 import './lessonTranscriptButtons.css'
+import './lessonNotes.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -14,6 +16,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <PasswordRecovery />
     <LessonTranscriptButtons />
     <TranscriptLibrary />
+    <LessonNotes />
     <AnswerBalancer />
   </React.StrictMode>,
 )
