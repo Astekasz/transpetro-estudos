970075e92e36c39385e4b639845ce22a703cde99
Astekasz <armando_rocha_@hotmail.com>
2026-09-11 10:15:31 -0300

    feat: integra banco focado em degravacoes e Transpetro 2023

--- src/main.jsx
+++ src/main.jsx
@@ -12,6 +12,7 @@ import AnswerFeedbackNormalizer from './AnswerFeedbackNormalizer'
 import ExtraStudyOptions from './ExtraStudyOptions'
 import { questions } from './data/questions'
 import { transcriptQuestions } from './data/transcriptQuestions'
+import { focusedQuestions } from './data/focusedQuestions'
 import './styles.css'
 import './simulationSelection.css'
 import './answerConfirmation.css'
@@ -19,8 +20,11 @@ import './lessonTranscriptButtons.css'
 import './lessonNotes.css'
 
 const existingQuestionIds = new Set(questions.map(question => question.id))
-transcriptQuestions.forEach(question => {
-  if (!existingQuestionIds.has(question.id)) questions.push(question)
+;[...transcriptQuestions, ...focusedQuestions].forEach(question => {
+  if (!existingQuestionIds.has(question.id)) {
+    questions.push(question)
+    existingQuestionIds.add(question.id)
+  }
 })
 
 ReactDOM.createRoot(document.getElementById('root')).render(
