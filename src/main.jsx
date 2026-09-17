89dd15e58abca0cbca106ea25bc674c5476ab411
Astekasz <armando_rocha_@hotmail.com>
2026-09-17 17:07:43 -0300

    feat: integra botão para refazer prova

--- src/main.jsx
+++ src/main.jsx
@@ -11,6 +11,7 @@ import AnswerConfirmation from './AnswerConfirmation'
 import AnswerFeedbackNormalizer from './AnswerFeedbackNormalizer'
 import ExtraStudyOptions from './ExtraStudyOptions'
 import LastExamTab from './LastExamTab'
+import ExamRetakeButton from './ExamRetakeButton'
 import { questions } from './data/questions'
 import { transcriptQuestions } from './data/transcriptQuestions'
 import { focusedQuestions } from './data/focusedQuestions'
@@ -20,6 +21,7 @@ import './answerConfirmation.css'
 import './lessonTranscriptButtons.css'
 import './lessonNotes.css'
 import './lastExam.css'
+import './examRetake.css'
 
 const existingQuestionIds = new Set(questions.map(question => question.id))
 ;[...transcriptQuestions, ...focusedQuestions].forEach(question => {
@@ -56,5 +58,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <AnswerFeedbackNormalizer />
     <ExtraStudyOptions />
     <LastExamTab />
+    <ExamRetakeButton />
   </React.StrictMode>,
 )
