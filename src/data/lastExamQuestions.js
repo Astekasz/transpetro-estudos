2ecdf924621b94adca7f7c767c8ce87dd4c73205
Astekasz <armando_rocha_@hotmail.com>
2026-09-17 11:10:32 -0300

    feat: consolida questões reais da prova Transpetro 2023

---
+++ src/data/lastExamQuestions.js
@@ -0,0 +1,10 @@
+import { lastExamQuestions21to35 } from './lastExamQuestions21to35'
+import { lastExamQuestions36to50 } from './lastExamQuestions36to50'
+import { lastExamQuestions51to70 } from './lastExamQuestions51to70'
+import { q46Figure } from './q46Figure'
+
+export const lastExamQuestions = [
+  ...lastExamQuestions21to35,
+  ...lastExamQuestions36to50,
+  ...lastExamQuestions51to70
+].map(question => question.number === 46 ? { ...question, figure: q46Figure } : question)
