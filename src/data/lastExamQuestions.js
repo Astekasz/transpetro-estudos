a1cfd0e6674fd22e6a6a1fd53341662d1de97442
Astekasz <armando_rocha_@hotmail.com>
2026-09-17 11:13:01 -0300

    feat: inclui prova completa com 70 questões

--- src/data/lastExamQuestions.js
+++ src/data/lastExamQuestions.js
@@ -1,9 +1,11 @@
+import { lastExamQuestions1to20 } from './lastExamQuestions1to20'
 import { lastExamQuestions21to35 } from './lastExamQuestions21to35'
 import { lastExamQuestions36to50 } from './lastExamQuestions36to50'
 import { lastExamQuestions51to70 } from './lastExamQuestions51to70'
 import { q46Figure } from './q46Figure'
 
 export const lastExamQuestions = [
+  ...lastExamQuestions1to20,
   ...lastExamQuestions21to35,
   ...lastExamQuestions36to50,
   ...lastExamQuestions51to70
