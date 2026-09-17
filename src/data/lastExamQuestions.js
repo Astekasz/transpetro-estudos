93000cd831d760e8d68a211c4de2bfde8be92258
Astekasz <armando_rocha_@hotmail.com>
2026-09-17 11:18:41 -0300

    feat: vincula explicações conceituais à prova 2023

--- src/data/lastExamQuestions.js
+++ src/data/lastExamQuestions.js
@@ -2,6 +2,7 @@ import { lastExamQuestions1to20 } from './lastExamQuestions1to20'
 import { lastExamQuestions21to35 } from './lastExamQuestions21to35'
 import { lastExamQuestions36to50 } from './lastExamQuestions36to50'
 import { lastExamQuestions51to70 } from './lastExamQuestions51to70'
+import { lastExamExplanations } from './lastExamExplanations'
 import { q46Figure } from './q46Figure'
 
 export const lastExamQuestions = [
@@ -9,4 +10,8 @@ export const lastExamQuestions = [
   ...lastExamQuestions21to35,
   ...lastExamQuestions36to50,
   ...lastExamQuestions51to70
-].map(question => question.number === 46 ? { ...question, figure: q46Figure } : question)
+].map(question => ({
+  ...question,
+  explanation: lastExamExplanations[question.number] || '',
+  ...(question.number === 46 ? { figure: q46Figure } : {})
+}))
