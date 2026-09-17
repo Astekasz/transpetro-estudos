8be95eb45689ac516120ee9a89a0d1e258d06fbf
Astekasz <armando_rocha_@hotmail.com>
2026-09-17 10:58:27 -0300

    feat: integra aba Prova 2023 ao site

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,7 @@ import ErrorNotebookDetails from './ErrorNotebookDetails'
 import AnswerConfirmation from './AnswerConfirmation'
 import AnswerFeedbackNormalizer from './AnswerFeedbackNormalizer'
 import ExtraStudyOptions from './ExtraStudyOptions'
+import LastExamTab from './LastExamTab'
 import { questions } from './data/questions'
 import { transcriptQuestions } from './data/transcriptQuestions'
 import { focusedQuestions } from './data/focusedQuestions'
@@ -18,6 +19,7 @@ import './simulationSelection.css'
 import './answerConfirmation.css'
 import './lessonTranscriptButtons.css'
 import './lessonNotes.css'
+import './lastExam.css'
 
 const existingQuestionIds = new Set(questions.map(question => question.id))
 ;[...transcriptQuestions, ...focusedQuestions].forEach(question => {
@@ -53,5 +55,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <AnswerConfirmation />
     <AnswerFeedbackNormalizer />
     <ExtraStudyOptions />
+    <LastExamTab />
   </React.StrictMode>,
 )
